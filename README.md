e21533a221987c4820db3b5e32e4cc8f86913820
Jaime <jaimeloeuf@gmail.com>
2021-08-22 21:10:46 +0800

    CLI command to accept arguments:     Accept arguments to change directory to deploy and allow user to     customise the commit message.

--- README.md
+++ README.md
@@ -2,11 +2,34 @@
 Simple CLI tool to deploy to github pages
 
 
+## Usage
+### CLI Options
+- `--dir`
+    - Pass in a directory other than the default `./docs` to deploy
+- `--message`
+    - Pass a custom commit message instead of the default `[Github pages deployment commit]`
+    - Message must be wrapped in quotes if longer than a single string
+
+### Using from CLI
+```bash
+# Install as dev dependency
+npm install -D simple-github-pages-deploy
+
+# Use npx to run it
+# Uses all the default options
+npx simple-github-pages-deploy
+```
+
 ### Add to npm scripts
 ```json
 "scripts": {
     "deploy": "npx simple-github-pages-deploy"
 },
+```
+
+### Using from CLI without default options
+```bash
+npx simple-github-pages-deploy --dir ./dist --message "github pages deployment"
 ```
 
 
--- main.js
+++ main.js
@@ -1,10 +1,36 @@
-const childProcess = require("child_process");
+// Function to deploy using data from options object
+// Where, Options: { dir: String, message: String }
+function deploy(options) {
+  const childProcess = require("child_process");
 
-// Add ./docs which is the build output directory
-childProcess.execSync("git add ./docs");
+  childProcess.execSync(`git add ${options.dir}`);
 
-// Must use double quotes for the commit message, else will fail in powershell
-childProcess.execSync('git commit -m "[Github pages deployment commit]"');
+  // Must use double quotes for the commit message, else will fail in powershell
+  childProcess.execSync(`git commit -m "${options.message}"`);
 
-// Deploy by pushing the commit to github
-childProcess.execSync("git push");
+  // Deploy by pushing the commit to github
+  childProcess.execSync("git push");
+}
+
+// Returns CLI argument value or the given default value
+function getArgWithDefault(args, argString, defaultValue) {
+  const argIndex = args.findIndex((arg) => arg === `--${argString}`);
+  return argIndex === -1 ? defaultValue : args[argIndex + 1];
+}
+
+// Get CLI arguments after `node` and current file's name
+const args = process.argv.slice(2);
+
+deploy(
+  // Get options values from CLI arguments if available
+  {
+    // ./docs is the default github pages directory choice if not using the repo root
+    dir: getArgWithDefault(args, "dir", "./docs"),
+
+    message: getArgWithDefault(
+      args,
+      "message",
+      "[Github pages deployment commit]"
+    ),
+  }
+);
